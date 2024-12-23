5ffb5a5cf9183981adde54480078febae375b95e
Timothy Greene <timothygreene@Timothys-MacBook-Pro.local>
2024-12-23 12:20:32 -0500

    Initial commit

---
+++ src/main/java/com/greene/greene_demo/GreeneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.greene.greene_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreeneDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greene/greene_demo/GreeneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greene.greene_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeneDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
